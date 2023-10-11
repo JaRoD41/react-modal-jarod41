57265ebd3024e2e8e714cef0144aab5574bae99d
JaRoD41 <laurent.trinco@gmail.com>
2023-10-11 11:45:17 +0200

    Modal component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import { useState } from 'react'
+import { Modal } from './src/components/Modal'
 
 const App = () => {
-  return (
-    <div>
-      <h1>App</h1>
-    </div>
-  )
+	const [isOpen, setIsOpen] = useState(true)
+	return (
+		<div>
+			<Modal
+				isOpen={isOpen}
+				onClose={function () {
+					setIsOpen(false)
+				}}
+				children={
+					<div>
+						<h1>Titre de la modale</h1>
+						<p>Contenu de la modale</p>
+					</div>
+				}
+			/>
+		</div>
+	)
 }
 
 export default App

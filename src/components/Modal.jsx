dbf54ce963f70c9656847c56efdc95db6e9c2292
JaRoD41 <laurent.trinco@gmail.com>
2023-11-17 10:48:41 +0100

    update component

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import './Modal.css'
 
 /**
  * Renders a modal component with the given properties.
@@ -8,25 +8,21 @@ import React from 'react'
  * @param {ReactNode} children - The content to be rendered inside the modal.
  * @return {ReactNode} The rendered modal component.
  */
-const Modal = (isOpen, onClose, children) => {
-  if (!isOpen) {
+const Modal = ({ isOpen, onClose, children }) => {
+	if (!isOpen) {
 		return null
 	}
-
 	return (
-		<div className="modal fixed w-full h-full top-0 left-0 flex items-center justify-center">
-			<div className="modal-overlay absolute w-full h-full bg-gray-900 opacity-50"></div>
-			<div className="modal-container bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto">
-				<div className="modal-content py-4 text-left px-6">
+		<div id='modal' className='modal'>
+			<div className='modal-overlay'></div>
+			<div className='modal-container'>
+				<div className='modal-content'>
 					{/* <!--Content--> */}
-					<div className="my-5">{children}</div>
+					<div className='content'>{children}</div>
 					{/* <!--Footer--> */}
-					<div className="flex justify-end pt-2">
-						<button
-							onClick={onClose}
-							className="focus:outline-none modal-close px-4 bg-gray-400 p-3 rounded-lg text-black hover:bg-gray-300"
-						>
-							Fermer
+					<div className='footer'>
+						<button onClick={onClose} className='close-button'>
+							Close
 						</button>
 					</div>
 				</div>

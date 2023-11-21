fc8988f901adbe94e567ea286829d9b04f29d03e
JaRoD41 <laurent.trinco@gmail.com>
2023-11-21 09:48:03 +0100

    export OK modal

--- dist/Modal.mjs
+++ dist/Modal.mjs
@@ -1,4 +1,5 @@
-const c = ({ isOpen: e, onClose: a, children: t }) => e ? /* @__PURE__ */ React.createElement("div", { id: "modal", className: "modal" }, /* @__PURE__ */ React.createElement("div", { className: "modal-overlay" }), /* @__PURE__ */ React.createElement("div", { className: "modal-container" }, /* @__PURE__ */ React.createElement("div", { className: "modal-content" }, /* @__PURE__ */ React.createElement("div", { className: "content" }, t), /* @__PURE__ */ React.createElement("div", { className: "footer" }, /* @__PURE__ */ React.createElement("button", { onClick: a, className: "close-button" }, "Close"))))) : null;
+import e from "react";
+const m = ({ isOpen: a, onClose: t, children: l }) => a ? /* @__PURE__ */ e.createElement("div", { id: "modal", className: "modal" }, /* @__PURE__ */ e.createElement("div", { className: "modal-overlay" }), /* @__PURE__ */ e.createElement("div", { className: "modal-container" }, /* @__PURE__ */ e.createElement("div", { className: "modal-content" }, /* @__PURE__ */ e.createElement("div", { className: "content" }, l), /* @__PURE__ */ e.createElement("div", { className: "footer" }, /* @__PURE__ */ e.createElement("button", { onClick: t, className: "close-button" }, "Close"))))) : null;
 export {
-  c as default
+  m as default
 };
